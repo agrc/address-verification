187f0853a19fcbc6f85ff1799cf6668e84fdfa9e
stdavis <stdavis@utah.gov>
2024-12-18 16:47:37 -0700

    chore: point at production masquerade

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   masqueradeUrl:
-    'https://masquerade-1081956331224.us-central1.run.app/arcgis/rest/services/UtahLocator/GeocodeServer/findAddressCandidates',
+    'https://masquerade.ugrc.utah.gov/arcgis/rest/services/UtahLocator/GeocodeServer/findAddressCandidates',
 };
 
 export default config;
